b512ea880bc51c9330fb1d65577cbd20ced700f7
Daniel Chinedu <chinedudaniel7@gmail.com>
2016-10-22 15:02:25 +0100

    Add signup controller

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,28 @@
+class SignupController < ApplicationController
+  def email
+  end
+
+  def name
+  end
+
+  def photo
+  end
+
+  def location
+  end
+
+  def skills
+  end
+
+  def role
+  end
+
+  def template
+  end
+
+  def color
+  end
+
+  def custom_url
+  end
+end
