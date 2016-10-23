cc0ecfffdac716a5c753f6aebfb98c711556a3af
Daniel Chinedu <chinedudaniel7@gmail.com>
2016-10-23 13:09:28 +0100

    update signup controller

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,14 +1,31 @@
 class SignupController < ApplicationController
   def email
+    if request.post?
+      user = User.new(params[:user])
+      redirect_to signup_path('name')
+
+    end
   end
 
   def name
+    if request.post?
+
+      redirect_to signup_path('photo')
+    end
   end
 
   def photo
+    if request.post?
+
+      redirect_to signup_path('name')
+    end
   end
 
   def location
+    if request.post?
+
+      redirect_to signup_path('location')
+    end
   end
 
   def skills
